f378e74a8d5e702134ae1fedfbbdeef9fe16db3c
Rolodophone <rolodophone@gmail.com>
2024-10-03 10:25:41 +0100

    lab 2 task 1 (convolution function)

---
+++ Lab2-number-plate-challenge/task1.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+
+def convolution(input_image, kernel):
+    output_image = np.zeros(input_image.shape, dtype=np.uint8)
+    kernel_radius = round((kernel.shape[0] - 1) / 2)  # note: this assumes kernel is square
+    input_padded = cv2.copyMakeBorder(input_image, kernel_radius, kernel_radius, kernel_radius, kernel_radius,
+                                      cv2.BORDER_REPLICATE)
+
+    for y in range(input_image.shape[0]):
+        for x in range(input_image.shape[1]):
+            patch = input_padded[y:y + kernel.shape[0], x:x + kernel.shape[1]]
+            output_image[y, x] = (np.multiply(patch, kernel)).sum()
+
+    return output_image
+
+
+input_image = cv2.imread("mandrill.jpg", cv2.IMREAD_GRAYSCALE)
+kernel = np.ones((3, 3)) / 9
+
+cv2.imshow("Result", convolution(input_image, kernel))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
